19526b9ef2f4395710e655941a26d6aa9774004f
Pradeep Selakoti <pradeepselakoti@gmail.com>
2025-08-25 17:06:57 +0530

    sandbox

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,10 +1,17 @@
 import {  gemini, createAgent} from "@inngest/agent-kit"
 import { inngest } from "./client";
+import {Sandbox} from "@e2b/code-interpreter"
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async()=>{
+      const sandbox = await Sandbox.create("vibe-nextjs-prdeep");
+      return sandbox.sandboxId;
+    })
+
     const codeAgent = createAgent({
       name: "code-agent",
       system: "you are an expert summarizer.You summarize in 2 words",
@@ -14,6 +21,12 @@ model: gemini({ model: "gemini-2.0-flash-exp" })
     const { output } = await codeAgent.run(
     `Summarize the following text: ${event.data.value}`,
 );
-    return {output };
+
+const sandboxUrl = await step.run("get-sandbox-url", async()=>{
+  const sandbox = await getSandbox(sandboxId);
+  const host = sandbox.getHost(3000)
+  return `https://${host}`;
+})
+    return {output, sandboxUrl };
   },
 );
