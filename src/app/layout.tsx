6debf158b3b999b2f5467167ec26ddb7e08c39fe
Pradeep Selakoti <pradeepselakoti@gmail.com>
2025-08-28 21:13:21 +0530

    project-header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ThemeProvider } from "next-themes";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { TRPCReactProvider } from "@/trpc/client";
@@ -26,15 +27,23 @@ export default function RootLayout({
 }>) {
   return (
     <TRPCReactProvider>
-       <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Toaster/>
-        {children}
-      </body>
-    </html>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ThemeProvider
+           attribute="class"
+           defaultTheme="system"
+           enableSystem
+           disableTransitionOnChange
+          >
+            <Toaster />
+            {children}
+          </ThemeProvider>
+
+        </body>
+      </html>
     </TRPCReactProvider>
-   
+
   );
 }
